84edaa8718ac7327e755ccb9a7a2dfe11e62a1d5
Tikhon <tihon.savchuk@gmail.com>
2020-05-22 16:01:38 +0300

    Updates

---
+++ precache-manifest.0732ca2e36327e165684d931e5cf39c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb58b1ec091031cc623aa6382b879dfb",
+    "url": "/tihon-veliar/test-task-blog/index.html"
+  },
+  {
+    "revision": "b67ca81819702718cf0c",
+    "url": "/tihon-veliar/test-task-blog/static/css/main.af1018e8.chunk.css"
+  },
+  {
+    "revision": "90e76672d3b029457060",
+    "url": "/tihon-veliar/test-task-blog/static/js/2.a1d691f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tihon-veliar/test-task-blog/static/js/2.a1d691f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b67ca81819702718cf0c",
+    "url": "/tihon-veliar/test-task-blog/static/js/main.0ffc44ee.chunk.js"
+  },
+  {
+    "revision": "30eea5fc94ef4039513d",
+    "url": "/tihon-veliar/test-task-blog/static/js/runtime-main.be4ff223.js"
+  }
+]);
